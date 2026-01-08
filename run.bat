2c9cb92d80c61c3b50828a7de5f1b503a4ac9d3d
m1rzz <miramecho@gmail.com>
2026-01-08 19:55:47 +0200

    run.bat troubleshooting

--- run.bat
+++ run.bat
@@ -1,4 +1,18 @@
 @echo off
+echo Checking for Python...
+python --version >nul 2>&1
+if errorlevel 1 (
+    echo ERROR: Python is not installed!
+    echo Opening Python download page...
+    start https://www.python.org/downloads/
+    echo.
+    echo Please install Python and check "Add Python to PATH"
+    echo Then run this file again.
+    pause
+    exit /b
+)
+
+echo Starting server...
 cd /d "%~dp0"
 start http://localhost:8000
 python -m http.server 8000
